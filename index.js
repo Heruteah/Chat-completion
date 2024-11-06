e55be78614c06fcee1c79d14fa4f2270c6a2d280
Heruko Zhen <162998538+Heruteah@users.noreply.github.com>
2024-11-06 09:48:52 +0000

    Hello world

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
-app.use(express.static('public')); 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
